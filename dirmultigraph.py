b9f9f82ad5d015da4386bb7253b8a9632aad9edd
Justin Wang <justinwang46@gmail.com>
2022-09-20 23:46:58 -0400

    start of transition

---
+++ dirmultigraph.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import json
+
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize, sent_tokenize
+# stop_words = set(stopwords.words('english'))
+
+class GraphVisualization():
+    def __init__(self):
+        self.graph = nx.Graph()
+    
+    def generate_graph_dict(self, sentence):
+        tokenized = sent_tokenize(sentence)
+        for i in tokenized:
+            wordsList = nltk.word_tokenize(i)
+
+        wordsList = [w for w in wordsList if not w in stop_words]
+        outputs = nltk.pos_tag(wordsList)
+
+        # self.edges = {('Anshul', 'dogs'): ["loves"]}
+        # self.nodes = {"Anshul": [("Anshul", "dogs")], "dogs": [("Anshul", "dogs")]}
+
+        nouns = []
+        relationship = ""
+
+        for word, label in outputs:
+            if "NN" in label or "PRP" in label:
+                nouns += [word]
+            elif "JJ" in label or "VB" in label:
+                relationship = word
+        
+        for i in range(len(nouns)-1):
+            self.append_data([(nouns[i], nouns[i+1]), relationship])
+            
+
+    def append_data(self, edge_connection):
+        # edge_connection structure: [(node1, node2), relationship]
+
+        # update nodes
+        node1 = edge_connection[0][0]
+        node2 = edge_connection[0][1]
+
+        if node1 not in self.nodes.keys():
+            self.nodes[node1] = [edge_connection[0]]
+        else:
+            self.nodes[node1].append(edge_connection[0])
+        
+        if node2 not in self.nodes.keys():
+            self.nodes[node2] = [edge_connection[0]]
+        else:
+            self.nodes[node2].append(edge_connection[0])
+
+        # update edges
+        if edge_connection[0] not in self.edges.keys():
+            self.edges[edge_connection[0]] = [edge_connection[1]]
+        else:
+            self.edges[edge_connection[0]].append(edge_connection[1])
+    
+    def pull_data(self, node):
+        if node in self.nodes.keys():
+            relationships = self.nodes[node]
+            return_sequence = []
+            #outputs a list of sentences which can be directly tokenized and prepended to prompt
+            for i in range(len(relationships)):
+                for j in self.edges[relationships[i]]:
+                    return_sequence += [relationships[i][0] + " " + j + " " + relationships[i][1]]
+            return return_sequence
+        return []        
+
+    def draw_graph(self):
+        self.graph.add_edges_from(self.edges.keys())
+        pos = nx.spring_layout(self.graph)
+
+        fig, ax = plt.subplots(figsize=(12,12))
+        nx.draw(
+            self.graph, pos, edge_color='#696969', width=1, linewidths=1,
+            node_size=500, node_color='#696969', alpha=0.9, font_color="#b0b0b0",
+            labels={node: node for node in self.graph.nodes()}
+        )
+        nx.draw_networkx_edge_labels(
+            self.graph, pos,
+            edge_labels=self.edges,
+            font_color='#b0b0b0',
+            bbox=dict(facecolor='#121212', edgecolor='#121212'))
+        ax.set_facecolor('#121212')
+        ax.axis('off')
+        fig.set_facecolor('#121212')
+        plt.show()
+
--- run.py
+++ run.py
@@ -1,68 +1,90 @@
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
-import tensorflow as tf
-import spacy
+import torch
+from transformers import GPT2Tokenizer, GPT2Model
+# import spacy
+import networkx as nx
+import pickle
+import argparse
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-stop_words = set(stopwords.words('english'))
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize, sent_tokenize
+# stop_words = set(stopwords.words('english'))
 
-from graph import GraphVisualization
+# from graph import GraphVisualization
 
+def run(cfg):
+    # load model
+    # tokenizer = GPT2Tokenizer.from_pretrained('microsoft/DialoGPT-large')
+    # model = GPT2Model.from_pretrained('microsoft/DialoGPT-large')
 
-tokenizer = GPT2Tokenizer.from_pretrained('microsoft/DialoGPT-large')
-model = TFGPT2LMHeadModel.from_pretrained('microsoft/DialoGPT-large')
+    # load graph
+    if cfg.graph is not None:
+        assert 'p' and 'k' and 'l' in args.graph, 'convo must be a path to a pickle file'
+        with open(args.graph, 'rb') as f:
+            graph = pickle.load(f)
+        assert isinstance(graph, nx.MultiDiGraph), 'object must be a MultiDiGraph'
+    else:
+        graph = nx.MultiDiGraph()
 
+    visualization = GraphVisualization()
 
-visualization = GraphVisualization()
+    visualization.generate_graph_dict("I am a vegetarian")
+    visualization.generate_graph_dict("I have an Android")
+    visualization.generate_graph_dict("I love learning")
+    visualization.generate_graph_dict("I loved the weather")
+    visualization.generate_graph_dict("I am happy today")
+    visualization.generate_graph_dict("I love my family")
+    visualization.generate_graph_dict("I love my friends")
 
-visualization.generate_graph_dict("I am a vegetarian")
-visualization.generate_graph_dict("I have an Android")
-visualization.generate_graph_dict("I love learning")
-visualization.generate_graph_dict("I loved the weather")
-visualization.generate_graph_dict("I am happy today")
-visualization.generate_graph_dict("I love my family")
-visualization.generate_graph_dict("I love my friends")
+    # visualization.draw_graph()
 
-# visualization.draw_graph()
+    step = 0
+    while step < cfg.n_steps:
+        # encode the new user input, add the eos_token and return a tensor in Tensorflow
+        input_text = input(">> User: ")
 
-step = 0
-while step < 15:
-    # encode the new user input, add the eos_token and return a tensor in Tensorflow
-    input_text = input(">> User: ")
+        tokenized = sent_tokenize(input_text)
+        for i in tokenized:
+            wordsList = nltk.word_tokenize(i)
+            wordsList = [w for w in wordsList if not w in stop_words]
 
-    tokenized = sent_tokenize(input_text)
-    for i in tokenized:
-        wordsList = nltk.word_tokenize(i)
-        wordsList = [w for w in wordsList if not w in stop_words]
+            outputs = nltk.pos_tag(wordsList)
 
-        outputs = nltk.pos_tag(wordsList)
+        nouns = []
+        for word, label in outputs:
+            if "NN" in label or "PRP" in label:
+                nouns += [word]
+        
+        context_arr = [visualization.pull_data(noun) for noun in nouns]
+        context_string = ""
 
-    nouns = []
-    for word, label in outputs:
-        if "NN" in label or "PRP" in label:
-            nouns += [word]
-    
-    context_arr = [visualization.pull_data(noun) for noun in nouns]
-    context_string = ""
+        for i in context_arr:
+            for j in i:
+                context_string += j + " "
 
-    for i in context_arr:
-        for j in i:
-            context_string += j + " "
+        new_user_input_ids = tokenizer.encode(context_string + input_text + tokenizer.eos_token, return_tensors='tf')
 
-    new_user_input_ids = tokenizer.encode(context_string + input_text + tokenizer.eos_token, return_tensors='tf')
+        visualization.generate_graph_dict(input_text)
 
-    visualization.generate_graph_dict(input_text)
+        # append the new user input tokens to the chat history
+        bot_input_ids = tf.concat([chat_history_ids, new_user_input_ids], axis=-1) if step > 0 else new_user_input_ids
 
-    # append the new user input tokens to the chat history
-    bot_input_ids = tf.concat([chat_history_ids, new_user_input_ids], axis=-1) if step > 0 else new_user_input_ids
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-    # generated a response while limiting the total chat history to 1000 tokens, 
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        # pretty print last ouput tokens from bot
+        print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
 
-    # pretty print last ouput tokens from bot
-    print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+        step += 1
 
-    step += 1
+    visualization.draw_graph()
 
-visualization.draw_graph()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, default='convo')
+    parser.add_argument('--graph', type=str, default=None)
+    parser.add_argument('--n_steps', type=int, default=15)
+    parser.add_argument('--save_dir', type=str, default='convos/')
+
+    args = parser.parse_args()
+    run(args)
